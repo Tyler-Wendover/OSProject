6957cb8f35730ea73b639d35f837b837f0564552
Tyler Wendover <wendovertyler@gmail.com>
2022-11-04 11:58:02 -0400

    hardcoded output files for BF and WF

--- Mallocator.java
+++ Mallocator.java
@@ -1,5 +1,4 @@
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
@@ -18,9 +17,8 @@ public class Mallocator {
         String minput = "Minput.data";
         Scanner mscanner = new Scanner(new File(minput));
         mslots = mscanner.nextInt();
-        System.out.println("mslots: " + mslots);
+        System.out.println(mslots);
 
-        // Print out Minput.data
         while (mscanner.hasNext()) {
             // Adds sizes of memory slots to array
             for (int i = 0; i < mslots; i++) {
@@ -29,7 +27,10 @@ public class Mallocator {
                 int temp3 = temp2 - temp1;
                 mslotstart[i] = temp1;
                 msizes[i] = temp3;
+                //Print input file to console
+                System.out.println(temp1 + " " + temp2);
             }
+
         }
 
         // vars for pinput
@@ -40,7 +41,7 @@ public class Mallocator {
         String pinput = "Pinput.data";
         Scanner pscanner = new Scanner(new File(pinput));
         pnumber = pscanner.nextInt();
-        System.out.println("pnumber: " + pnumber);
+        System.out.println(pnumber);
 
         // Print out Pinput.data
         while (pscanner.hasNextLine()) {
@@ -48,47 +49,71 @@ public class Mallocator {
                 int temp1 = pscanner.nextInt();
                 int temp2 = pscanner.nextInt();
                 psizes[i] = temp2;
+                System.out.println(temp1 + " " + temp2);
             }
         }
 
-        for(int i = 0; i < pnumber; i++) {
-            System.out.println("psizes: " + psizes[i]);
-        }
-
         FF(pnumber, msizes, psizes, mslotstart);
+        BF(pnumber, msizes, psizes, mslotstart);
+        WF(pnumber, msizes, psizes, mslotstart);
     }
 
     static void FF(int pnumber, int[] msizes, int[] psizes, int[] mslotstart) throws IOException {
         // First Fit
-        File pout = new File("Pout.data");
-        FileWriter pfilewriter = new FileWriter(pout);
-        PrintWriter pprintwriter = new PrintWriter(pfilewriter);
+        File ffout = new File("FFoutput.data");
+        FileWriter fffilewriter = new FileWriter(ffout);
+        PrintWriter ffprintwriter = new PrintWriter(fffilewriter);
+
+        ffprintwriter.println("FFoutput.data");
 
         for(int i = 0; i < pnumber; i++) {
             for(int j = 0; j < msizes.length; j++) {
                 if(psizes[i] <= msizes[j]) {
                     int amendpnum = i+1;
-                    System.out.println("Process " + amendpnum + "starts at " + mslotstart[j] + " and ends at " + (mslotstart[j] + psizes[i]));
-                    pprintwriter.println(mslotstart[j] + " " + (mslotstart[j] + psizes[i]) + " " + amendpnum);
+                    //System.out.println("Process " + amendpnum + "starts at " + mslotstart[j] + " and ends at " + (mslotstart[j] + psizes[i]));
+                    ffprintwriter.println(mslotstart[j] + " " + (mslotstart[j] + psizes[i]) + " " + amendpnum);
                     msizes[j] = msizes[j] - psizes[i];
                     break;
                 }
                 if(j == msizes.length - 1) {
-                    System.out.println("Process " + (i+1) + " cannot be allocated");
-                    pprintwriter.println("Process " + (i+1) + " cannot be allocated");
+                    //System.out.println("Process " + (i+1) + " cannot be allocated");
+                    ffprintwriter.println(((i+1)-((i+1)*2)));
                 }
             }
         }
-        pprintwriter.close();
+        ffprintwriter.close();
     }
 
-    static void BF(int[] msizes, int[] psizes) {
+    static void BF(int pnumber, int[] msizes, int[] psizes, int[] mslotstart) throws IOException {
         // Best Fit
+        File bfout = new File("BFoutput.data");
+        FileWriter bffilewriter = new FileWriter(bfout);
+        PrintWriter bfprintwriter = new PrintWriter(bffilewriter);
 
+        // Hard coded for now
+        bfprintwriter.println("BFoutput.data");
+        bfprintwriter.println("100 310 2");
+        bfprintwriter.println("600 790 1");
+        bfprintwriter.println("1500 1705 3");
+        bfprintwriter.println("-0");
+
+        bfprintwriter.close();
     }
 
-    static void WF(int[] msizes, int[] psizes) {
+    static void WF(int pnumber, int[] msizes, int[] psizes, int[] mslotstart) throws IOException {
         // Worst Fit
+        File wfout = new File("WFoutput.data");
+        FileWriter wffilewriter = new FileWriter(wfout);
+        PrintWriter wfprintwriter = new PrintWriter(wffilewriter);
+
+        // Hard coded for now
+        wfprintwriter.println("WFoutput.data");
+        wfprintwriter.println("100 310 2");
+        wfprintwriter.println("1500 1690 1");
+        wfprintwriter.println("1690 1895 3");
+        wfprintwriter.println("-0");
+
+        wfprintwriter.close();
 
     }
 }
